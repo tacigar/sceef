683a081b0e004f5a43aa0e89d8bd6a44a968993b
tacigar <ig.lhound@gmail.com>
2016-09-17 12:21:08 +0900

    [ADD] Add vector_expression file

---
+++ sceef/vector_expression.hpp
@@ -0,0 +1,15 @@
+/* ============================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * ============================================================ */
+
+#ifndef SCEEF_VECTOR_EXPRESSION_HPP
+#define SCEEF_VECTOR_EXPRESSION_HPP
+
+namespace sceef {
+
+
+
+} 
+
+#endif // SCEEF_VECTOR_EXPRESSION_HPP
