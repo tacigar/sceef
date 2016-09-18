6a158c685b3246fc62553bdba803bc2d167f1465
tacigar <ig.lhound@gmail.com>
2016-09-19 08:59:09 +0900

    [ADD] Add unary_operation file

---
+++ sceef/unary_operation.hpp
@@ -0,0 +1,13 @@
+/* ============================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * ============================================================ */
+
+#ifndef SCEEF_UNARY_OPERATION_HPP
+#define SCEEF_UNARY_OPERATION_HPP
+
+namespace sceef {
+
+} // namespace sceef
+
+#endif // SCEEF_UNARY_OPERATION_HPP
