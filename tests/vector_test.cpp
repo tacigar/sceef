4bc39d06ee7311a328d61502da840aad34fad6c5
tacigar <ig.lhound@gmail.com>
2016-09-19 08:41:19 +0900

    [ADD] Add VectorAddTest

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -16,3 +16,24 @@ TEST(VectorTest, VectorSizeTest) {
     }
 }
 
+TEST(VectorTest, VectorAddTest) {
+    struct TestCase {
+        sceef::Vector<int, 3> lhs;
+        sceef::Vector<int, 3> rhs;
+        sceef::Vector<int, 3> res;
+    };
+
+    std::vector<TestCase> tests = {
+        {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}},
+        {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+        {{10, 10, 10}, {-10, -10, -10}, {0, 0, 0}},
+    };
+    
+    for (const auto& test :tests) {
+        // for evaluation, type must be placed.
+        sceef::Vector<int, 3> tmp = test.lhs + test.rhs;
+        for (int i = 0; i < 3; i++) {
+            ASSERT_EQ(tmp[i], test.res[i]);
+        }
+    }
+}
