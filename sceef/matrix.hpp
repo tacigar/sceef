1b3f701e11d6e75a75037a1493d7aad2da714d4c
tacigar <ig.lhound@gmail.com>
2016-09-19 09:49:26 +0900

    [ADD] Add matrix file

---
+++ sceef/matrix.hpp
@@ -0,0 +1,15 @@
+/* ============================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * ============================================================ */
+
+#ifndef SCEEF_MATRIX_HPP
+#define SCEEF_MATRIX_HPP
+
+namespace sceef {
+
+
+} // namespace sceef
+
+#endif // SCEEF_MATRIX_HPP
+
