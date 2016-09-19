4eacf9163ebee17ed813c246b2e53d92507752f6
tacigar <ig.lhound@gmail.com>
2016-09-19 19:31:09 +0900

    [ADD] Add Matrix Test file

---
+++ tests/matrix_test.cpp
@@ -0,0 +1,12 @@
+/* ============================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * ============================================================ */
+
+#include <vector>
+#include <utility>
+#include <gtest/gtest.h>
+#include <sceef/matrix.hpp>
+#include <sceef/matrix_expression.hpp>
+
+
