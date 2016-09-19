12dba0f97bd9dd4fb5df24bdea65d56c5cbee89e
tacigar <ig.lhound@gmail.com>
2016-09-19 15:20:22 +0900

    [UPDATE] Update Matrix class

--- sceef/matrix.hpp
+++ sceef/matrix.hpp
@@ -8,8 +8,44 @@
 
 namespace sceef {
 
+template <class T, int N, int M>
+class Matrix
+        : public MatrixExpression<Matrix<T, N, M> >
+{
+  private:
+    using RowType = std::array<T, M>;
+    using StorageType = std::array<RowType, N>;
+    
+  public:
+    Matrix()
+            : storage_()
+    {
+    }
+
+    template <class Expression>
+    Matrix(const MatrixExpression<Expression>& expression)
+            : storage_()
+    {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                stroage_[i][j] = expression[i][j];
+            }
+        }
+    }
+
+    RowType& operator [] (int index) {
+        return storage_[index];
+    }
+
+    const RowType& operator [] (int index) const {
+        return storage_[index];
+    }
+
+  private:
+    StorageType storage_;
+};
 
 } // namespace sceef
 
-#endif // SCEEF_MATRIX_HPP
+#endif // oSCEEF_MATRIX_HPP
 
