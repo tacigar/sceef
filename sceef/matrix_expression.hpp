0a64d63e011df2f08ebaca6509133312feac1ae7
tacigar <ig.lhound@gmail.com>
2016-09-19 15:21:25 +0900

    [ADD] Add Matrix Expression file

---
+++ sceef/matrix_expression.hpp
@@ -0,0 +1,14 @@
+/* ============================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * ============================================================ */
+
+#ifndef SCEEF_MATRIX_EXPRESSION_HPP
+#define SCEEF_MATRIX_EXPRESSION_HPP
+
+namespace sceef {
+
+} // namespace sceef
+
+#endif // SCEEF_MATRIX_EXPRESSION_HPP
+
