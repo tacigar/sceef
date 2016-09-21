e20a00fbda1ab073080e4ce6198d9e51504570a4
tacigar <ig.lhound@gmail.com>
2016-09-21 16:32:47 +0900

    [ADD] Add Matrix unary operation

--- sceef/unary_operation.hpp
+++ sceef/unary_operation.hpp
@@ -29,7 +29,6 @@ class VectorUnaryOperation
     const Operand& operand_;
 };
 
-
 template <class Operand>
 auto operator + (const VectorExpression<Operand>& operand) {
     return VectorUnaryOperation<Positive, Operand>(operand());
@@ -38,6 +37,40 @@ auto operator + (const VectorExpression<Operand>& operand) {
 template <class Operand>
 auto operator - (const VectorExpression<Operand>& operand) {
     return VectorUnaryOperation<Negative, Operand>(operand());
+}
+
+// ============================================================
+
+template <class Operator, class Operand>
+class MatrixUnaryOperation
+        : public MatrixExpression<MatrixUnaryOperation<Operator, Operand> >
+{
+  public:
+    static constexpr int ROW_SIZE = Operand::ROW_SIZE;
+    static constexpr int COLUMN_SIZE = Operand::COLUMN_SIZE;
+
+  public:
+    MatrixUnaryOperation(const Operand& operand)
+            : operand_(operand)
+    {
+    }
+
+    auto at(int i, int j) const -> decltype(auto) {
+        return Operator::apply(operand_.at(i, j));
+    }
+    
+  private:
+    const Operand& operand_;
+};
+
+template <class Operand>
+auto operator + (const MatrixExpression<Operand>& operand) {
+    return MatrixUnaryOperation<Positive, Operand>(operand());
+}
+
+template <class Operand>
+auto operator - (const MatrixExpression<Operand>& operand) {
+    return MatrixUnaryOperation<Negative, Operand>(operand());
 }
 
 } // namespace sceef
