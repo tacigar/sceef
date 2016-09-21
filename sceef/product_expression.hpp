38b1d11955c180343f91006e78cd72a36198ef07
tacigar <ig.lhound@gmail.com>
2016-09-21 23:31:43 +0900

    [ADD] Add product expression file

---
+++ sceef/product_expression.hpp
@@ -0,0 +1,13 @@
+/* ============================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * ============================================================ */
+
+#ifndef SCEEF_PRODUCT_EXPRESSION_HPP
+#define SCEEF_PRODUCT_EXPRESSION_HPP
+
+namespace sceef {
+
+} // namespace sceef
+
+#endif // SCEEF_PRODUCT_EXPRESSION_HPP
