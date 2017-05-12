d22a7e301fb8824e201337272c36762b0d9550e5
tacigar <ig.lhound@gmail.com>
2017-05-12 23:19:01 +0900

    Add vector tags.

---
+++ sceef/row_vector_tag.hpp
@@ -0,0 +1,18 @@
+/* =============================================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * 
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ * ============================================================================= */
+
+#ifndef SCEEF_ROW_VECTOR_TAG_HPP
+#define SCEEF_ROW_VECTOR_TAG_HPP
+
+namespace sceef {
+
+	class row_vector_tag {};
+	
+} // namespace sceef
+
+#endif // SCEEF_ROW_VECTOR_TAG_HPP
---
+++ sceef/vector_tag.hpp
@@ -0,0 +1,18 @@
+/* =============================================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ * ============================================================================= */
+
+#ifndef SCEEF_VECTOR_TAG_HPP
+#define SCEEF_VECTOR_TAG_HPP
+
+namespace sceef {
+
+	class vector_tag {};
+	
+} // namespace sceef
+
+#endif // SCEEF_VECTOR_TAG_HPP
