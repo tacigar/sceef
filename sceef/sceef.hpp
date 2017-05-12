8faa013cc74a36340e3c1bde2dd8c0f90af7ef07
tacigar <ig.lhound@gmail.com>
2017-05-12 23:23:39 +0900

    Add header file.

---
+++ sceef/sceef.hpp
@@ -0,0 +1,44 @@
+/* =============================================================================
+ * Copyright (c) 2016 tacigar. All rights reserved.
+ * https://github.com/tacigar/sceef
+ * 
+ * Distributed under the Boost Software License, Version 1.0. (See accompanying
+ * file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ * ============================================================================= */
+
+#ifndef SCEEF_SCEEF_HPP
+#define SCEEF_SCEEF_HPP
+
+/*
+#include <sceef/matrix.hpp>
+#include <sceef/matrix_bop.hpp>
+#include <sceef/matrix_expression.hpp>
+#include <sceef/matrix_prod.hpp>
+#include <sceef/matrix_row.hpp>
+#include <sceef/matrix_row_bop.hpp>
+#include <sceef/matrix_row_expression.hpp>
+#include <sceef/matrix_row_uop.hpp>
+#include <sceef/matrix_uop.hpp>
+*/
+
+#include <sceef/equals.hpp>
+#include <sceef/matrix.hpp>
+#include <sceef/matrix_bop.hpp>
+#include <sceef/matrix_expression.hpp>
+#include <sceef/matrix_uop.hpp>
+#include <sceef/op.hpp>
+#include <sceef/row_vector.hpp>
+#include <sceef/row_vector_bop.hpp>
+#include <sceef/row_vector_expression.hpp>
+#include <sceef/row_vector_uop.hpp>
+#include <sceef/vector.hpp>
+#include <sceef/vector_base.hpp>
+#include <sceef/vector_base_bop.hpp>
+#include <sceef/vector_base_expression.hpp>
+#include <sceef/vector_base_uop.hpp>
+#include <sceef/vector_bop.hpp>
+#include <sceef/vector_expression.hpp>
+#include <sceef/vector_tag.hpp>
+#include <sceef/vector_uop.hpp>
+
+#endif // SCEEF_SCEEF_HPP
